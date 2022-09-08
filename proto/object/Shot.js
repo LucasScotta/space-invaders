327a6f9f23e4e03348b16488c3bbb16e9c9f65d2
LucasScotta <lucasmanuelscotta@gmail.com>
2022-09-08 19:21:11 -0300

    shot hitbox fixed

--- proto/object/Shot.js
+++ proto/object/Shot.js
@@ -3,42 +3,42 @@
 define(['proto/object/util/setup-element'], (setupElement) => class Shot {
     constructor(elem) {
         this.$el = setupElement('shot')
-        this.pos = {x: elem.pos.x + elem.size.width/2, y: elem.pos.y}
-        this.size = {width: 2, height: 16}
+        this.pos = { x: elem.pos.x + elem.size.width / 2, y: elem.pos.y }
+        this.size = { width: 2, height: 16 }
         this.shotDir = elem.shotDir
         this.paint()
     }
     isHitting(elem) {
-        return (this.isHittingTop(elem) || this.isHittingBottom(elem))
+        const upOrDown = this.isHittingTop(elem) || this.isHittingBottom(elem)
+        const rightOrLeft = this.isHittingRight(elem) || this.isHittingLeft(elem)
+        return upOrDown && rightOrLeft
     }
     isHittingTop(elem) {
         const elemTop = elem.pos.y
         const elemBottom = elemTop + elem.size.height
-        const elemLeft = elem.pos.x
-        const elemRight = elemLeft + elem.size.width
-
-        const top = this.pos.y
-        const left = this.pos.x
-        const right = left + this.size.width
-        return top >= elemTop && top <= elemBottom && right >= elemLeft && left <= elemRight
+        return this.pos.y >= elemTop && this.pos.y <= elemBottom
     }
     isHittingBottom(elem) {
+        const bottom = this.pos.y + this.size.height
         const elemTop = elem.pos.y
         const elemBottom = elemTop + elem.size.height
+        return bottom >= elemTop && bottom <= elemBottom
+    }
+    isHittingLeft(elem) {
+        const elemLeft = elem.pos.x
+        const elemRight = elem.pos.x + elem.size.width
+        return this.pos.x >= elemLeft && this.pos.x <= elemRight
+    }
+    isHittingRight(elem) {
         const elemLeft = elem.pos.x
         const elemRight = elemLeft + elem.size.width
-
-        const top = this.pos.y
-        const bottom = top + this.size.height
-        const left = this.pos.x
-        const right = left + this.size.width
-        return bottom >= elemTop && bottom <= elemBottom && right >= elemLeft && left <= elemRight
+        return this.pos.x + this.size.width >= elemLeft && this.pos.x + this.size.width <= elemRight
     }
     shot(game) {
         game.addShot(Shot())
     }
     /**
-    /**
+     *
      * Remove element from HTML
      */
     remove() {
